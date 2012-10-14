95f37fe3a78a5b93cf7282596d6b74e968f4518a
Nacho <imgarayojeda@gmail.com>
2012-10-14 17:06:59 -0300

    tiene un par de basuras, pero el loguear anda

--- src/LoguearV5/LoguearV5.sh
+++ src/LoguearV5/LoguearV5.sh
@@ -17,28 +17,29 @@ errcode=
 errstat=
 cmdname=
 sep=";"
+GRUPO="."
+LOGSIZE=2000000
 
-while getopts "c:f:i:hs:" opt
-do
-	case $opt in
+
+while getopts c:f:i:hs: opt
+do 
+	case "$opt" in
 		h)
-			 uso; exit 1
-		;;
+			uso; exit 1;;
 		c)
 			errcode=$OPTARG
-		;;
+			;;
 		f)
 			cmdname=$OPTARG
-		;;
+			;;
 		i)
 			errstat=$OPTARG
-		;;
+			;;
 		s)
 			sep=$OPTARG
-		;;	
-		?)
+			;;	
+		\?)
 			uso; exit 1
-		;;
 	esac
 done
 
@@ -51,25 +52,29 @@ if [ -z $LOGDIR ]; then
 fi
 if [ -z $LOGEXT ]; then
 	LOGEXT="log"
+fi
 if [ ! -d $LOGDIR ]; then
 	mkdir $LOGDIR
 fi
 
 if [ ! -z $LOGSIZE ]; then
-	$LOGSIZE=100000
+	LOGSIZE=100000
 fi
 
 output="$cmdname.$LOGEXT"
 
-if [ `stat -c%s $LOGDIR/$output` -gt $LOGSIZE ]; then
+if [ -r $LOGDIR/$output ] && [ `stat -c%s $LOGDIR/$output` -gt $LOGSIZE ]; then
 	tail -n `expr `wc -l $LOGDIR/$output`/2` > $LOGDIR/$output
 	sh LoguearV5.sh -c 1 -f $cmdname -i A
 fi
 
-mensaje=`grep $errcode ListaErrores`
-fecha=`date +"%D"`
-usr=`gawk '{print $1}' FS=" "`
 
-printf "%s$sep%s$sep%s$sep%s$sep%120s\n" $fecha $usr $errstat $cmdname $mensaje >>$LOGDIR/$output
+mensaje=`grep "$errcode" ListaErrores`
+printf "%s" "$mensaje"
+fecha=`date +"%D"`
+
+usr=`who | gawk '{print $1}' FS=" " | sed s/[^$]*//`
+printf "usr = %s\n" "$usr"
+printf "%s$sep%s$sep%s$sep%s$sep%s\n" "$fecha" "$usr" "$errstat" "$cmdname" "$mensaje" >>$LOGDIR/$output
 
 exit 0
