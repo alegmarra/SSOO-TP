2c8afe7b0c07205494d7d299dabef57d308ca4e9
Migue <mat1204@hotmail.com>
2012-10-15 21:48:01 -0300

    Agregado lista de errores y correcion de iniciarv5

--- src/IniciarV5/IniciarV5.sh
+++ src/IniciarV5/IniciarV5.sh
@@ -1,32 +1,36 @@
 #!/bin/bash 
 
-#esta es la posta: 
-CONFIG_FILE="../conf/InstalaV5.conf"
+#@todo: dejar sólo la válida, esta es la posta:
+#@todo: decirle a Migue que el Iniciar va en $GRUPO
+CONFIG_FILE="./conf/InstalaV5.conf"
 #test: 
-CONFIG_FILE="InstalaV5.conf"
+#CONFIG_FILE="InstalaV5.conf"
 VARIABLES=(GRUPO CONFDIR BINDIR MAEDIR ARRIDIR ACEPDIR RECHDIR PROCDIR REPODIR LOGDIR LOGEXT)
-COMANDOS=(IniciarV5 DetectaV5 BuscarV5 ListarV5 MoverV5 LoguearV5 MirarV5 StopD StartD)
+COMANDOS=(DetectaV5 BuscarV5 ListarV5 MoverV5 LoguearV5 MirarV5 StopD StartD)
 ARCHIVOS_MAE=(patrones sistemas)
 # Variables para informar al usuario
 HEADER="TP SO7508 Segundo Cuatrimestre 2012. Tema V Copyright © Grupo 07"
-declare -A DESCRIPCIONES
-DESCRIPCIONES[CONFDIR]="Librería del Sistema: "
-DESCRIPCIONES[BINDIR]="Ejecutables: "
-DESCRIPCIONES[MAEDIR]="Archivos maestros: "
-DESCRIPCIONES[ARRIDIR]="Directorio de arribo de archivos externos: "
-DESCRIPCIONES[ACEPDIR]="Archivos externos aceptados: "
-DESCRIPCIONES[RECHDIR]="Archivos externos rechazados: "
-DESCRIPCIONES[PROCDIR]="Archivos procesados: "
-DESCRIPCIONES[REPODIR]="Reportes de salida: "
-DESCRIPCIONES[LOGDIR]="Logs de auditoría del sistema: "
+DESCRIPCIONES=()
+DESCRIPCIONES[0]=$HEADER
+DESCRIPCIONES[1]="Librería del Sistema: "
+DESCRIPCIONES[2]="Ejecutables: "
+DESCRIPCIONES[3]="Archivos maestros: "
+DESCRIPCIONES[4]="Directorio de arribo de archivos externos: "
+DESCRIPCIONES[5]="Archivos externos aceptados: "
+DESCRIPCIONES[6]="Archivos externos rechazados: "
+DESCRIPCIONES[7]="Archivos procesados: "
+DESCRIPCIONES[8]="Reportes de salida: "
+DESCRIPCIONES[9]="Logs de auditoría del sistema: "
+###
+CONFDIR="."
+BINDIR="../IniciarV5"
+MAEDIR="../../src/IniciarV5"
 
 mostrarDescripcionYListarArchivos () {
-	echo ${DESCRIPCIONES[$1]} ${!1}
-	cd ${!1}
-	ls -l | awk '{ print $9 }' | grep --color=never '..*' # grep para evitar líneas vacías		
-	cd ~-
+	echo ${DESCRIPCIONES[$1]} ${!VARIABLES[${i}]}
+	ls -l ${!VARIABLES[${1}]} | awk '{ print $8 }' | grep --color=never '..*' # grep para evitar líneas vacías		
 }
- 
+
 # Verifica que un proceso de nombre $1 esté corriendo, y guarda en $2 el PID
 # $1 nombre de proceso
 # $2 variable para almacenar el PID
@@ -44,16 +48,15 @@ mostrarVariables () {
 
 	for i in {1..3}
 	do
-		mostrarDescripcionYListarArchivos ${VARIABLES[$i]}
+		mostrarDescripcionYListarArchivos ${i}
 	done
 
 	for ((i = 4; i < ${#VARIABLES[@]} - 2; ++i))
 	do
-		TEMP=${VARIABLES[$i]}	
-    	echo ${DESCRIPCIONES[$TEMP]} ${!TEMP}
+		echo ${DESCRIPCIONES[${i}]} ${VARIABLES[$i]}
 	done
 
-	echo ${DESCRIPCIONES[LOGDIR]} "$LOGDIR/<comando>.$LOGEXT"
+	echo ${DESCRIPCIONES[9]} "$LOGDIR/<comando>.$LOGEXT"
 }
 
 # Verifica si todas las variables de ambiente están seteadas. 
@@ -88,8 +91,6 @@ init () {
 	then
 		verificarSiYaSeInicioElEntorno $1
 	fi	
-
-	echo "Comando IniciarV5 Inicio de Ejecución @todo: log"
 }
 
 setearVariablesDeEntorno () {
@@ -103,20 +104,19 @@ setearVariablesDeEntorno () {
 verificarSiLaInstalacionEstaCompleta () {
 	local FALTANTES=()
 	local i=0
-	cd ${BINDIR}
+	
 	for CMD in ${COMANDOS[@]}
 	do
-		ls | grep ${CMD} > /dev/null
+		ls ${BINDIR} | grep ${CMD} > /dev/null
 		if [ $? -eq 1 ]
 		then
 			FALTANTES[((i++))]=${CMD}
 		fi
 	done
 
-	cd ${MAEDIR}
 	for ARCH in ${ARCHIVOS_MAE[@]}
 	do
-		ls | grep ${ARCH} > /dev/null
+		ls ${MAEDIR} | grep ${ARCH} > /dev/null
 		if [ $? -eq 1 ]
 		then
 			FALTANTES[((i++))]=${ARCH}
@@ -127,8 +127,8 @@ verificarSiLaInstalacionEstaCompleta () {
 	then
 		echo $HEADER
 		echo "Componentes Existentes:"
-		mostrarDescripcionYListarArchivos ${BINDIR}
-		mostrarDescripcionYListarArchivos ${MAEDIR}
+		mostrarDescripcionYListarArchivos 2
+		mostrarDescripcionYListarArchivos 3
 		echo "Componentes faltantes:"
 		for i in ${FALTANTES[@]}
 		do
@@ -146,12 +146,12 @@ invocarDetecta () {
 	then
 		PID=0
 		verificarProceso "DetectaV5.sh" ${PID}
-		echo "Demonio corriendo bajo el Nro: ${PID}"
+		${BINDIR}/LoguearV5.sh -c 102 -f IniciarV5 -i A ${PID}
 	fi
 }
 
 fin () {
-	echo "Proceso de Inicialización Concluido @todo: loguear y cerrar"
+	${BINDIR}/LoguearV5.sh -c 103 -f IniciarV5 -i
 }
 
 ### MAIN ###
@@ -162,7 +162,9 @@ verificarSiYaSeInicioElEntorno
 
 setearVariablesDeEntorno
 
-export PATH=${PATH}:`pwd`; echo "@todo: revisar si está bien la variable PATH"
+${BINDIR}/LoguearV5.sh -c 101 -f IniciarV5
+
+export PATH=${PATH}:${BINDIR}; echo "@todo: revisar si está bien la variable PATH"
 
 verificarSiLaInstalacionEstaCompleta
 
