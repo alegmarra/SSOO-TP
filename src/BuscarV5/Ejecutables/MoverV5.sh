2584e2635d74d838f0e984bc3a99be36629358b9
Javier Choque <javier.choque@outlook.com>
2012-10-15 23:42:45 -0300

    BuscarV5 pasa las pruebas

--- src/BuscarV5/Ejecutables/MoverV5.sh
+++ src/BuscarV5/Ejecutables/MoverV5.sh
@@ -53,18 +53,39 @@ argumentosValidos () {
 	if [ "$origenDir" == "$destinoDir" ]; then 
 		# Directorio destino igual al origen
 		# Inválido
+                if $loguear ; then
+
+                        $BINDIR/LoguearV5.sh -c "605" -i "E" -f "MoverV5.sh" "$destino"
+                fi
+
 		return 1 
 	else 
 		if [ ! -e "$1" ]; then
 			if [ ! -d "$origenDir" ]; then
 				# No existe directorio de origen
+		                if $loguear ; then
+	
+        		                $BINDIR/LoguearV5.sh -c "604" -i "I" -f "MoverV5.sh" \
+							"$origen"
+                		fi
 				return 1
 			fi
+			
 			# No existe archivo de origen
+		        if $loguear ; then
+	
+        			$BINDIR/LoguearV5.sh -c "604" -i "I" -f "MoverV5.sh" \
+							"$origen"
+               		fi
 			return 1
 		fi	
 	
 		if [ ! -d "$destinoDir" ]; then
+		        if $loguear ; then
+	
+        			$BINDIR/LoguearV5.sh -c "604" -i "I" -f "MoverV5.sh" \
+							"$destino"
+               		fi
 			# No existe directorio de destino
 			return 1
 		fi
@@ -135,6 +156,8 @@ if [[ "$#" -lt 3 ]]; then
 	echo "Cantidad de argumentos inválida"
 	ayuda 
 
+        $BINDIR/LoguearV5.sh -c "004" -i "SE" -f "MoverV5.sh" "3" "$#"
+
 	exit 1
 fi
 
@@ -174,6 +197,12 @@ if [[ "$sonValidos" -eq 0 ]]; then
 
 		# renombra el archivo
 		mv "$origen" "$origenNext"
+
+		if $loguear ; then 
+
+			$BINDIR/LoguearV5.sh -c "606" -i "I" -f "MoverV5.sh" "$origen" "$origenNext"
+		fi
+
 		# realiza el movimiento
 		mv "$origenNext" "$destino"
 	fi
@@ -182,15 +211,19 @@ if [[ "$sonValidos" -eq 0 ]]; then
 		# no pudo mover el archivo, retorna con codigo de error
 		if $loguear ; then 
 
-			$BINDIR/LoguearV5.sh -c "010" -f "MoverV5.sh" -i "SE"
-
+			$BINDIR/LoguearV5.sh -c "603" -i "SE" -f "MoverV5.sh" "$origen"
 		fi
 		exit 1
+	else
+		if $loguear ; then 
+
+			$BINDIR/LoguearV5.sh -c "602" -i "I" -f "MoverV5.sh" "$origen"
+		fi
 	fi	
 else
 	# Rutas de origen y/o destino inválidas
 	if $loguear ; then 
-		$BINDIR/LoguearV5.sh -c "104" -f "MoverV5.sh" -i "E"
+		$BINDIR/LoguearV5.sh -c "005" -i "SE" -f "MoverV5.sh"
 	fi
 	exit 1
 fi
