422091b7cd8590231b3c8643c90c1eab8d9f3528
Javier Choque <javier.choque@outlook.com>
2012-10-16 00:43:11 -0300

    Agrego las pruebas del BuscarV5 (son medio chotas pero sirven)

--- src/BuscarV5/test.sh
+++ src/BuscarV5/test.sh
@@ -10,6 +10,7 @@ RECHDIR=./Rechazados
 # Mover todos los archivos de la carpeta de procesados al comienzo
 mv ./Procesados/*_* ./Aceptados/ > /dev/null
 mv ./Rechazados/*_* ./Aceptados/ > /dev/null
+rm ./Procesados/*
 
 export BINDIR
 export ACEPDIR
@@ -22,10 +23,60 @@ export RECHDIR
 
 COD_RES=$?
 
-echo "-----------------------------------------"
 echo "Codigo devuelto por BuscarV5.sh: $COD_RES"
 
-if [ $COD_RES -eq 2 ]; then
-	echo "Tal vez, algun editor de texto o IDE tiene abierto el script:"
-	ps ax | grep -v grep | grep "BuscarV5.sh"
+if [ -e $PROCDIR/resultados.8 ]; then
+	echo "OK"
+else
+	echo "ERROR"
+fi
+
+if [ -e $PROCDIR/resultados.9 ]; then
+	echo "OK"
+else
+	echo "ERROR"
+fi
+
+if [ -e $PROCDIR/rglobales.8 ]; then
+	echo "OK"
+else
+	echo "ERROR"
+fi
+
+if [ -e $PROCDIR/rglobales.9 ]; then
+	echo "OK"
+else
+	echo "ERROR"
+fi
+
+read -r linea < ./Procesados/rglobales.8
+if echo "$linea" | grep -E "*,1,5,1" > /dev/null
+then
+	echo "OK"
+else
+	echo "ERROR"
+fi
+
+read -r linea < ./Procesados/rglobales.9
+if echo "$linea" | grep -E "*,2,3,1" > /dev/null
+then
+	echo "OK"
+else
+	echo "ERROR"
+fi
+
+read -r linea < ./Procesados/resultados.8
+if echo "$linea" | grep -E "*cielo$" > /dev/null
+then
+	echo "OK"
+else
+	echo "ERROR"
+fi
+
+read -r linea < ./Procesados/resultados.9
+if echo "$linea" | grep -E "*rojo$" > /dev/null
+then
+	echo "OK"
+else
+	echo "ERROR"
 fi
